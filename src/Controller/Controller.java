aea0edee92f731f0b6ffee393ae9b6e9485e4a2f
jlg <you@example.com>
2021-02-22 00:25:48 +0800

    修改提交

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,8 +5,8 @@ import Module.Module;
 public class Controller extends Module{
 	
 
-	public static void Controller(){
-	for(int i=0;i<c.length-1;i++) {//冒泡排序,给nun数组进行降序
+	public void Stor(){
+	for(int i=0;i<c.length-1;i++) {//冒泡排序,给c数组进行降序
 	for(int j=0;j<nun;j++) {
 		if(c[j]<c[j+1]) {
 			int temp =c[j];
@@ -26,6 +26,7 @@ public class Controller extends Module{
 		}
 		}
 	}
+	
 	for(int i=0;i<c.length-1;i++) {
 	for(int j=nun+5;j<c.length-1;j++) {//冒泡排序给nun+5数组后面数组进行升序
 		if(c[j]>c[j+1]) {
@@ -35,7 +36,6 @@ public class Controller extends Module{
 		}
 	}
 	}
-
 	System.out.println();
 	System.out.print("排序后C:");
 	for(int i=0;i<c.length;i++) {
---
+++ src/Service/Service.java
@@ -0,0 +1,19 @@
+package Service;
+
+import Controller.Controller;
+import Module.Module;
+
+public class Service {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Module s=new Controller();
+		s.Mod();
+		  Controller s1 = new Controller();
+		s1.Stor();
+//		Controller.Controller();
+		
+
+	}
+
+}
